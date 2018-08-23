b45ab22acd7baca4dbed729a981032edcb951466
root <root@ip-172-31-22-14.eu-west-2.compute.internal>
2018-08-23 12:38:16 +0000

    added resource that create a file with output

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output the IP Address of the Container
+output "IP Address" {
+  value = "${module.container.ip}"
+}
+
+output "Container Name" {
+  value = "${module.container.container_name}"
+}
+
