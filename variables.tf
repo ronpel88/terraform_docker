fe7d07fbbb8484dc5fb6efede27474986e484f73
root <root@ip-172-31-22-14.eu-west-2.compute.internal>
2018-08-23 12:13:42 +0000

    added terraform.tfvars file with variables values

--- variables.tf
+++ variables.tf
@@ -5,35 +5,19 @@ variable "env" {
 variable "image" {
   description = "Image for container"
   type = "map"
-  default = {
-    dev = "ghost:latest"
-    prod = "ghost:alpine"
-  }
 }
 
 variable "container_name" {
   description = "Name of container"
   type = "map"
-  default = {
-    dev = "dev_blog"
-    prod = "prod_blog"
-  }
 }
 
 variable "int_port" {
   description = "Internal port for container"
   type = "map"
-  default = {
-    dev = "2368"
-    prod = "2368"
-  }
 }
 
 variable "ext_port" {
   description = "External port for container"
   type = "map"
-  default = {
-    dev = "8080"
-    prod = "80"
-  }
 }
